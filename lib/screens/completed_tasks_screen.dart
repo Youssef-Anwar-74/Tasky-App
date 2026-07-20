3014862b7befe14baf5aa787861400e347e598ad
youssef <youssefelkarawy@gmail.com>
2026-07-20 23:18:44 +0300

    feat: add priority and completed task views

---
+++ lib/screens/completed_tasks_screen.dart
@@ -0,0 +1,94 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tasky_app/core/services/preferences_manager.dart';
+
+import '../models/task_model.dart';
+import '../widgets/task_list_widget.dart';
+
+class CompletedTasksScreen extends StatefulWidget {
+  const CompletedTasksScreen({super.key});
+
+  @override
+  State<CompletedTasksScreen> createState() => _CompletedTasksScreenState();
+}
+
+class _CompletedTasksScreenState extends State<CompletedTasksScreen> {
+  List<TaskModel> completeTasks = [];
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadTask();
+  }
+
+  void _loadTask() async {
+    setState(() {
+      isLoading = false;
+    });
+    final finalTask = PreferencesManager().getString("tasks");
+    if (finalTask != null) {
+      final taskAfterDecode = jsonDecode(finalTask) as List<dynamic>;
+      setState(() {
+        completeTasks = taskAfterDecode
+            .map((element) => TaskModel.fromJson(element))
+            .toList();
+        completeTasks = completeTasks.where((element) => element.isDone == true).toList();
+      });
+
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Text(
+            "Completed Tasks",
+            style: Theme.of(context).textTheme.labelSmall,
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: isLoading
+                ? Center(child: CircularProgressIndicator(color: Colors.white))
+                : TaskListWidget(
+              tasks: completeTasks,
+              onTap: (bool? value, int? index) async {
+                setState(() {
+                  completeTasks[index!].isDone = value ?? false;
+                });
+
+                final pref = await SharedPreferences.getInstance();
+
+                final allData =PreferencesManager().getString("tasks");
+
+                if (allData != null) {
+                  List<TaskModel> allDataList = (jsonDecode(allData) as List).map((element) => TaskModel.fromJson(element)).toList();
+                  final int newIndex = allDataList.indexWhere((element) => element.id == completeTasks[index!].id,);
+                  allDataList[newIndex] = completeTasks[index!];
+
+                  await PreferencesManager().setString("tasks", jsonEncode(allDataList));
+
+                  _loadTask();
+                }
+              },
+              emptyMessage: 'No Tasks Found',
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/screens/high_priority_screen.dart
@@ -0,0 +1,90 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../core/services/preferences_manager.dart';
+import '../models/task_model.dart';
+import '../widgets/task_list_widget.dart';
+
+class HighPriorityScreen extends StatefulWidget {
+  const HighPriorityScreen({super.key});
+
+  @override
+  State<HighPriorityScreen> createState() => _HighPriorityScreenState();
+}
+
+class _HighPriorityScreenState extends State<HighPriorityScreen> {
+  List<TaskModel> highPriorityTasks = [];
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadTask();
+  }
+
+  void _loadTask() async {
+    setState(() {
+      isLoading = true;
+    });
+    final finalTask = PreferencesManager().getString("tasks");
+    if (finalTask != null) {
+      final taskAfterDecode = jsonDecode(finalTask) as List<dynamic>;
+      setState(() {
+        highPriorityTasks = taskAfterDecode
+            .map((element) => TaskModel.fromJson(element))
+            .toList();
+        highPriorityTasks = highPriorityTasks.where((element) => element.isHighPriority).toList().reversed.toList();
+      });
+
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "High Priority Tasks",
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: isLoading
+            ? Center(child: CircularProgressIndicator(color: Colors.white))
+            : TaskListWidget(
+          tasks: highPriorityTasks,
+          onTap: (bool? value, int? index) async {
+            setState(() {
+              highPriorityTasks[index!].isDone = value ?? false;
+            });
+
+            final allData = PreferencesManager().getString("tasks");
+
+            if (allData != null) {
+              List<TaskModel> allDataList =
+              (jsonDecode(allData) as List)
+                  .map((element) => TaskModel.fromJson(element))
+                  .toList();
+
+              final newIndex = allDataList.indexWhere(
+                    (element) => element.id == highPriorityTasks[index!].id,
+              );
+              allDataList[newIndex] = highPriorityTasks[index!];
+
+              await PreferencesManager().setString("tasks" , jsonEncode(allDataList));
+              _loadTask();
+            }
+          },
+          emptyMessage: 'No Tasks Found',
+        ),
+      ),
+    );
+  }
+}
